99afce6f089308cb0e26c497029432487630dc93
twwright <twwright@gmail.com>
2020-08-09 16:53:04 -0500

    Set up initial Rails app

---
+++ config/environments/test.rb
@@ -0,0 +1,19 @@
+Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'dashdashhelp.com' }
+  config.cache_classes = false
+  config.action_view.cache_template_loading = true
+  config.eager_load = false
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=
+  }
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+  config.cache_store = :null_store
+  config.action_dispatch.show_exceptions = false
+  config.action_controller.allow_forgery_protection = false
+  config.active_storage.service = :test
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :test
+  config.active_support.deprecation = :stderr
+end
