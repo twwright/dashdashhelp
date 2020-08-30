b7f5a0ba987776dbf90ec04a4bc6815db67832a7
twwright <twwright@gmail.com>
2020-08-30 00:59:12 -0500

    Update sessions controller and user model to take omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,26 @@ class User < ApplicationRecord
   include Clearance::User
   validates :username, presence: true, uniqueness: true
 
+  has_many :authentications, dependent: :destroy
   has_many :questions, dependent: :destroy
   has_many :upvotes
   has_many :upvoted_questions, through: :upvotes, source: :question
+
+  def self.create_with_auth_and_hash(authentication, auth)
+    if user = find_by(email: auth["info"]["email"])
+      user.authentications << authentication
+    else
+      create! do |user|
+        user.username = auth["info"]["nickname"]
+        user.email = auth["info"]["email"]
+        user.authentications << authentication
+      end
+    end
+  end
+
+  def password_optional?
+    true
+  end
 
   def upvote(question)
     upvoted_questions << question
