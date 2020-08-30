634c38b678812d5ed631ad3f8ecd307de8b0ba43
twwright <twwright@gmail.com>
2020-08-30 00:59:57 -0500

    Update routes to handle omniauth callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
-  resources :users, only: [:create, :show] do
+  resources :users, only: [:create, :show, :edit] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
@@ -26,4 +26,5 @@ Rails.application.routes.draw do
   get "/sign_in" => "sessions#new", as: "sign_in"
   delete "/sign_out" => "sessions#destroy", as: "sign_out"
   get "/sign_up" => "users#new", as: "sign_up"
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 end
