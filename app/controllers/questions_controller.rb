6bf4ed938c1cad474eee545c002897e2d20757cb
twwright <twwright@gmail.com>
2020-08-30 00:58:20 -0500

    Update strong params to include title attribute

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,7 +11,7 @@ class QuestionsController < ApplicationController
   private
 
   def question_params
-    params.require(:question).permit(:body, :image)
+    params.require(:question).permit(:body, :title, :image)
   end
 
   def redirect_options_for(question)
