b2a25be5ca7d0518ac370b0b2284312a3629556c
twwright <twwright@gmail.com>
2020-08-30 22:06:04 -0500

    Add logic for error flows and new answer creation

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,10 @@
 class QuestionsController < ApplicationController
+
   def show
     @question = Question.find(params[:id])
+    if current_user
+      @answer = @question.answers.build
+    end
   end
 
   def new
@@ -12,8 +16,13 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    question = current_user.questions.create(question_params)
-    redirect_to question_path(question), redirect_options_for(question)
+    @question = current_user.questions.new(question_params)
+    @question.user = current_user
+    if @question.save
+      redirect_to question_path(@question), redirect_options_for(@question)
+    else
+      render 'new'
+    end
   end
 
   private
