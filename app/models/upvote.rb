ebc064e68660a57aeb0568d1ff4d13a0b6bf9ca2
twwright <twwright@gmail.com>
2020-09-06 17:17:16 -0500

    Modify to include answer not question

--- app/models/upvote.rb
+++ app/models/upvote.rb
@@ -1,6 +1,6 @@
 class Upvote < ApplicationRecord
   belongs_to :user
-  belongs_to :question
+  belongs_to :answer
 
-  validates :user_id, uniqueness: { scope: :question_id } # only one pair of user_id/question_id that is upvoted
+  validates :user_id, uniqueness: { scope: :answer_id } # only one pair of user_id/answer_id that is upvoted
 end
