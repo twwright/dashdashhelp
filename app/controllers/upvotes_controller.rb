1dbdb3006d8d17d184d76b9c0cc59c0d2be6fdcc
twwright <twwright@gmail.com>
2020-09-07 14:19:17 -0500

    Modify memoization method

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -3,16 +3,16 @@ class UpvotesController < ApplicationController
 
   def create
     current_user.upvote(answer)
-    redirect_to question_path(params[:question_id])
+    redirect_to question_path(answer.question_id)
   end
 
   def destroy
     current_user.unvote(answer)
-    redirect_to question_path(params[:question_id])
+    redirect_to question_path(answer.question_id)
   end
 
   private
   def answer
-    @_answer ||= Answer.find(params[:answer_id]) # would be answer_id if nested resource!
+    @_answer ||= Answer.find(params[:id]) # would be answer_id if nested resource!
   end
 end
