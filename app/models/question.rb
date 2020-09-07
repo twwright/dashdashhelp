e98b49c64697207535e99d3bf21bc090ef1396d4
twwright <twwright@gmail.com>
2020-09-07 14:20:39 -0500

    Update has_many to for join with source of users

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :user
+  has_many :very_helpful_people, through: :answers, source: :users
   has_one_attached :image
 
   validates :body, presence: true, length: { in: 1... 1000 }
@@ -8,4 +9,7 @@ class Question < ApplicationRecord
   default_scope { order(created_at: :desc) }
 
   delegate :username, to: :user # blocks law of demeter violation in _question using applicationrecord macro
+
+
 end
+
