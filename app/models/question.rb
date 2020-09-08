8bd26d7d2dd9f83c1a80ad0359a10e0fb57c0100
twwright <twwright@gmail.com>
2020-09-08 09:40:35 -0500

    Add custom scope for unanswered to questions model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,15 +1,18 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+
   belongs_to :user
-  has_many :answered_users, through: :answers, source: :users
+  has_many :respondents, through: :answers, source: :users
+
   has_one_attached :image
 
+  validates :title, presence: true, length: { in: 1... 100 }
   validates :body, presence: true, length: { in: 1... 1000 }
 
   default_scope { order(created_at: :desc) }
+  scope :unanswered, -> { where("answers_count = 0") }
 
-  delegate :username, to: :user # blocks law of demeter violation in _question using applicationrecord macro
-
+  delegate :username, to: :user # blocks law of demeter violation in _question
 
 end
 
