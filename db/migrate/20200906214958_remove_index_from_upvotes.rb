e5281d41a1968f18379bc9c4f182aff8112bda9e
Thomas Wright <twwright@gmail.com>
2020-09-14 21:15:15 +0000

    Fix naming convention error

--- db/migrate/20200906214958_remove_index_from_upvotes.rb
+++ db/migrate/20200906214958_remove_index_from_upvotes.rb
@@ -1,4 +1,4 @@
-class Removeindexfromupvotes < ActiveRecord::Migration[6.0]
+class RemoveIndexFromUpvotes < ActiveRecord::Migration[6.0]
   def up
         remove_index :upvotes, name: "index_upvotes_on_user_id_and_question_id"
   end
